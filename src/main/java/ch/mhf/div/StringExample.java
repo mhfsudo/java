1fcdb3cdcdef68da1034dedf990811d4da1463dc
Michael Friderich <michael@frideri.ch>
2020-12-13 09:43:39 +0100

    update String Example

--- src/main/java/ch/mhf/div/StringExample.java
+++ src/main/java/ch/mhf/div/StringExample.java
@@ -13,16 +13,19 @@ public class StringExample
         String s2 = "Lisa";
         String s3 = " Lisa ";
         String s4 = "This is Lisa";
+        String s5 = "123";
         Integer i3 = 123;
 
-        s1.equals(s2);                          //return true
-        s1.equals("Lisa");                      //return true
-        i3.toString();                          //return "123"
-        Integer.toString(123);                  //return "123"
-        s1.length();                            //return 3
+        s1.equals(s2);                          //return true - Strings vergleichen
+        s1.equals("Lisa");                      //return true - Strings vergleichen
+        i3.toString();                          //return "123" - Zahl in String konventieren
+        String.valueOf(i3);                     //return "123" - Zahl in String konventieren
+        Integer.toString(123);                //return "123" - Zahl in String konventieren
+        Integer.parseInt(s5);                   //return 123 - String in Zahl konventieren
+        s1.length();                            //return 3 - Länge des Strings
         s1.toLowerCase();                       //return lisa
         s1.toUpperCase();                       //return LISA
-        s3.trim();                              //return Lisa
+        s3.trim();                              //return Lisa - entfernt die Leerzeichen
         s4.indexOf("Lisa");                     //return 8
         s1.concat(s2);                          //return LisaLisa
 
