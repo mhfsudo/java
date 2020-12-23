41271e484ed2b84cdab8238489a801a9d59be67b
Michael Friderich <michael@frideri.ch>
2020-12-23 07:20:53 +0100

    update hashset example

--- src/main/java/ch/mhf/collection/HashSetExample.java
+++ src/main/java/ch/mhf/collection/HashSetExample.java
@@ -17,9 +17,26 @@ public class HashSetExample
     {
         //Menge
         HashSet<String> name = new HashSet<>();                 //Instanz der Klasse HashSet
+
+        //add()
         name.add("Lisa");
         name.add("Michael");
         name.add("Lisa");                                       //Objekt kommt nur einmal vor
+
         System.out.println(name);                               //output [Michael, Lisa] (Keine Ordnung)
+
+        for(String currentElement : name)
+        {
+            System.out.println(currentElement);                 //output Michael Lisa
+        }
+
+        //contains()
+        name.contains("Lisa");                                  //return true
+
+        //remove()
+        name.remove("Michael");
+
+        //clear()
+        name.clear();
     }
 }
