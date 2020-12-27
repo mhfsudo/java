f59d8d2795ee0db607c016025db340d1ab581b26
Michael Friderich <michael@frideri.ch>
2020-12-27 22:17:10 +0100

    update AbstractClass

--- src/main/java/ch/mhf/classes/AbstractClass.java
+++ src/main/java/ch/mhf/classes/AbstractClass.java
@@ -1,52 +1,98 @@
 package ch.mhf.classes;
 
-/*
-    Data abstraction is the process of hiding certain details and showing
-    only essential information to the user
-    Abstraction can be achieved with either abstract classes or interfaces
- */
-
-//Better Solution with Interface
-//SuperClass
-abstract class AbstractAnimal
+class SuperAbstractClass
 {
-    //Abstract method (without body)
-    public abstract void animalSound();
-    //Regular method    (with body)
-    public void sleep()
+    //attribute
+    char attribute1 = 'a';
+
+    //constructor
+    public SuperAbstractClass()
     {
-        System.out.println("Zzz");
+        System.out.println("A");
+    }
+
+    //method
+    public void doAction1()
+    {
+        System.out.println(1);
     }
 }
 
-//Subclass
-class Cow extends AbstractAnimal
+abstract class ClassAbstract extends SuperAbstractClass
 {
-    public void animalSound()
-    {
-        System.out.println("Muhhh");
-    }
-}
+    //attribute
+    char attribute2 = 'b';
 
-//SubClass
-class Pig extends AbstractAnimal
-{
-    public void animalSound()
+    //constructor is possible
+    public ClassAbstract()
     {
-        System.out.println("Wee wee");
+        super();
+        System.out.println("B");
     }
-}
 
-public class AbstractClass
-{
+    //method
+    public void doAction2()
+    {
+        System.out.println(2);
+    }
+
+    //abstract method (has no body)
+    abstract public void doAction3();
+
     public static void main(String[] args)
     {
-        Cow myCow = new Cow();
-        myCow.animalSound();                    //output Muhhh
-        myCow.sleep();                          //output Zzz
+        SuperAbstractClass object1 = new SuperAbstractClass();      //output A
+        //ClassAbstract object2 = new ClassAbstract();              //not possible
+        SubAbstractClass object3 = new SubAbstractClass();          //output A B C
 
-        Pig myPig = new Pig();
-        myPig.animalSound();                    //output Wee wee
-        myPig.sleep();                          //output Zzz
+        System.out.println("----------");
+
+        //SuperAbstractClass object4 = new ClassAbstract();         //not possible
+        SuperAbstractClass object5 = new SubAbstractClass();        //output A B C
+        //ClassAbstract object6 = new SuperAbstractClass();         //not possible
+        ClassAbstract object7 = new SubAbstractClass();             //output A B C
+        //SubAbstractClass object8 = new ClassAbstract();           //not possible
+        //SubAbstractClass object9 = new SuperAbstractClass();      //not possible
+
+        System.out.println("----------");
+
+        System.out.println(object1.attribute1);                     //output a
+        System.out.println(object3.attribute1);                     //output a
+        System.out.println(object3.attribute2);                     //output b
+        System.out.println(object3.attribute3);                     //output c
+
+        System.out.println("----------");
+
+        object1.doAction1();                                        //output 1
+        object3.doAction1();                                        //output 1
+        object3.doAction2();                                        //output 2
+        object3.doAction3();                                        //output 3
+        object3.doAction4();                                        //output 4
+    }
+}
+
+class SubAbstractClass extends ClassAbstract
+{
+    //attribute
+    char attribute3 = 'c';
+
+    //constructor
+    public SubAbstractClass()
+    {
+        super();
+        System.out.println("C");
+    }
+
+    //abstract method must be override
+    @Override
+    public void doAction3()
+    {
+        System.out.println(3);
+    }
+
+    //method
+    public void doAction4()
+    {
+        System.out.println(4);
     }
 }
