f04437e62c15c0f57e55688d994f193990778f9a
Michael Friderich <michael@frideri.ch>
2021-03-19 10:02:31 +0100

    add thread join example

---
+++ src/main/java/ch/mhf/multithreading/ThreadJoin.java
@@ -0,0 +1,34 @@
+package ch.mhf.multithreading;
+
+public class ThreadJoin implements Runnable
+{
+
+    @Override
+    public void run()
+    {
+        System.out.println("Start " + Thread.currentThread().getName());
+        try
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                System.out.println(i + " ");
+                Thread.sleep(500);
+            }
+        }catch (InterruptedException e)
+        {
+            //Flag erneut setzen und dadurch Abrruch ermöglichen
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Stop " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) throws InterruptedException
+    {
+        Thread thread1 = new Thread(new ThreadJoin(), "first thread");
+        Thread thread2 = new Thread(new ThreadJoin(), "second thread");
+
+        thread1.start();
+        thread1.join();
+        thread2.start();
+    }
+}
